c51fe3a056a28defc2e84af65b0b835619940400
pankaj <pankaj.krjha.cse17@itbhu.ac.in>
2021-06-14 23:37:56 +0545

    Added Chatbot feature

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home,getlocation
+from .views import home,getlocation,botrequest
 urlpatterns = [
     path('',home),
-    path('loc/',getlocation)
+    path('loc/',getlocation),
+    path('bot/',botrequest)
 ]
