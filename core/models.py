d7038d97042c0f31a6d80ad827b262823e2ea9bc
pankaj <pankaj.krjha.cse17@itbhu.ac.in>
2021-06-15 08:35:40 +0545

    added str

--- core/models.py
+++ core/models.py
@@ -6,3 +6,5 @@ class Location(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     ip=models.CharField(max_length=300,blank=True,null=True)
     gps=models.URLField(max_length=200,blank=True,null=True)
+    def __str__(self):
+            return self.created_at 
